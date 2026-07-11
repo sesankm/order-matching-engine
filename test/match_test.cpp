8a86160d73a01f800345d087f92b46a95d68e6bc
sesankm <26676400+sesankm@users.noreply.github.com>
2026-07-11 17:02:56 -0500

    add tests for matching engine

---
+++ test/match_test.cpp
@@ -0,0 +1,26 @@
+#include "order.hpp"
+#include "matching_engine.hpp"
+#include <cassert>
+
+// TODO: Add gtest later
+int main()
+{
+    MatchingEngine me {};
+
+    me.submitOrder(4.5, 10, OType::LIMIT, OSide::BUY);
+    me.submitOrder(5.6, 1, OType::LIMIT, OSide::BUY);
+    me.submitOrder(7.0, 2, OType::LIMIT, OSide::BUY);
+    me.submitOrder(8.0, 2, OType::LIMIT, OSide::BUY);
+
+    assert(me.lob.bids[Order::get_price(7.0)].size() == 1);
+    assert(me.lob.bids[Order::get_price(8.0)].size() == 1);
+    assert(me.lob.asks.empty() == true);
+
+    me.submitOrder(7.0, 20, OType::LIMIT, OSide::SELL);
+    assert(me.lob.bids[Order::get_price(7.0)].size() == 0);
+    assert(me.lob.bids[Order::get_price(8.0)].size() == 0);
+    assert(me.lob.asks.empty() == false);
+
+    me.submitOrder(3.0, 20, OType::LIMIT, OSide::SELL);
+    assert(me.lob.asks.contains(Order::get_price(3.0)) == true);
+}
