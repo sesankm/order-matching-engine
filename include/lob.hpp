7e628dbece58ac3ff76b8c613d751f140d45334b
sesankm <26676400+sesankm@users.noreply.github.com>
2026-07-11 11:01:47 -0500

    add bid/ask and lookup maps; update return types

--- include/lob.hpp
+++ include/lob.hpp
@@ -5,19 +5,25 @@
 #include <vector>
 #include "order.hpp"
 
+auto inline comparator = [](const auto& f1, const auto& f2) { return f1 > f2; };
+
 class Lob {
 public:
     Lob();
-    void addOrder();
-    void cancelOrder();
-    void updateOrder();
+    std::uint64_t addOrder(float price, long quantity, OType type, OSide side);
+    void cancelOrder(long id);
+    void updateOrder(long id);
 
-    void getOrder() const;
-    void getBestBid() const;
-    void getBestAsk() const;
+    Order getOrder(long id) const;
+    std::uint64_t getBestBid() const;
+    std::uint64_t getBestAsk() const;
 
 private:
-    std::map<long, std::vector<Order>> buy;
+    std::map<long, std::vector<Order>, decltype(comparator)> bids {comparator};
+    std::map<long, std::vector<Order>> asks;
+
+    // O(1) lookup, tells us where the order is
+    std::unordered_map<long, OSide> lookup; 
 };
 
 #endif
