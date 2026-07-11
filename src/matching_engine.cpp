9116b8ffaf8fe6bb127ad1263248a9c2e29ebeeb
sesankm <26676400+sesankm@users.noreply.github.com>
2026-07-11 18:37:42 -0500

    fix order book walk logic

--- src/matching_engine.cpp
+++ src/matching_engine.cpp
@@ -24,26 +24,28 @@ bool MatchingEngine::doesCross(OSide side, std::uint64_t price) {
         return best > 0 && price >= best;
     }
     else {
-        std::uint64_t best = lob.getBestAsk();
+        std::uint64_t best = lob.getBestBid();
         return best > 0 && price <= lob.getBestBid();
     }
 }
 
 // TODO: Add buy side handling
 void MatchingEngine::walkOppositeSide(OSide side, std::uint64_t price, long& quantity) {
-    for (auto& [bucket, orders] : lob.bids) {
+    for (auto& [bucket, b_orders] : lob.bids) {
         if (quantity <= 0 || bucket < price) { break; }
-        while (quantity > 0 && orders.size() > 0) {
-            if (orders.front().m_quantity > quantity) {
-                orders.front().m_quantity -= quantity;
+        while (quantity > 0 && b_orders.size() > 0) {
+            if (b_orders.front().m_quantity > quantity) {
+                b_orders.front().m_quantity -= quantity;
                 quantity = 0;
                 break;
             } else {
-                quantity -= orders.front().m_quantity;
-                std::cout << "Filled buy order " << orders.front().m_order_id << "\n";
-                orders.pop_front();
-                continue;
+                quantity -= b_orders.front().m_quantity;
+                b_orders.front().m_quantity = 0;
+                std::cout << "Filled buy order " << b_orders.front().m_order_id << " in at " << b_orders.front().m_price << "\n";
+                lob.cancelOrder(b_orders.front().m_order_id);
+                break;
             }
         }
     }
+    lob.cleanFilledOrders();
 }
